43688450683303fea073419e011e156d0d2f9ef9
Nova Eeken <nova@Novas-MacBook-Pro.local>
2018-05-17 11:09:45 +0200

    Added host in config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,28 @@
 module.exports = {
   entry: [
-		'./src/index.js'
+    './src/index.js',
   ],
   output: {
-		path: __dirname,
-		publicPath: '/',
-		filename: 'bundle.js'
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js',
   },
   mode: 'development',
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: {
-				  loader: "babel-loader"
-				}
-		  }
-		]
-	},
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
   devServer: {
-		historyApiFallback: true,
+    historyApiFallback: true,
     contentBase: './',
     port: process.env.PORT || 8080,
-  }
+    host: '0.0.0.0',
+  },
 };
